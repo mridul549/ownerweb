e6b4d49db24236bfa7d54262e05d923b0bd3caa6
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-06-25 17:28:24 +0530

    Signup page errors complete and loading added

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,14 +1,32 @@
 import {React, useState} from "react";
 import '../css/auth.css'
 import {Link, useNavigate} from 'react-router-dom'
+import Spinner from "./Spinner";
 
 function Signup() {
     const [credentials, setCredentials] = useState({ownerName:'',email: '', password: '',cpassword:''})
+    const [error, setError] = useState(false)
+    const [passError, setPassError] = useState(false)
+    const [invalidCredError, setInvalidCredError] = useState({error: false, message: ''})
+    const [loading, setLoading] = useState(false)
     let navigate = useNavigate()
+    
     const handleSubmit = async (event) => {
         event.preventDefault();
-        // const data = new FormData(event.currentTarget);
-        
+
+        if(credentials.ownerName.length===0 || credentials.email.length===0 || 
+           credentials.password.length===0 || credentials.cpassword.length===0){
+
+            if(credentials.cpassword!==credentials.password){
+                setPassError(true)
+            } else {
+                setError(true)
+            }
+            return
+        }
+
+        setLoading(true)
+        setInvalidCredError({error: false})
         const {ownerName,email,password}=credentials;
         const response = await fetch("https://flavr.tech/owner/signup", {
             method: "POST",
@@ -18,17 +36,28 @@ function Signup() {
             body: JSON.stringify(({ownerName,email,password}))
         })
         const json = await response.json()
-
+        setLoading(false)
         if(json.action==="Owner created and OTP Sent"){
-            // save token and redirect to dashboard
             navigate('/otp')
         } else {
-            alert("Invalid credentials")
+            if(json.message!==undefined && json.message!==null){
+                setInvalidCredError({error: true, message: json.message})
+            } else {
+                setInvalidCredError({error: true, message: "Internal Server error"})
+            }
         }
     };
 
     const onChange = (e) => {
         setCredentials({...credentials, [e.target.name]: e.target.value})
+        setInvalidCredError({error: false, message: ''})
+        if(e.target.name==="cpassword" && 
+            (credentials.cpassword!==credentials.password || 
+             credentials.cpassword.length>=credentials.password.length)){
+            setPassError(true)
+        } else {
+            setPassError(false)
+        }
     }
 
     return (
@@ -41,14 +70,30 @@ function Signup() {
                 <h1 className="signup-head">FlavR</h1>
                     <h3>Sign Up</h3>
                     <form action="" onSubmit={handleSubmit}>
-                        <div><input className="input-field shadow-sm" type="text" onChange={onChange} name="ownerName" placeholder="Enter your name" /></div>
-                        <div><input className="input-field shadow-sm" type="email" onChange={onChange} name="email" placeholder="Enter your email" /></div>
-                        <div><input className="input-field shadow-sm" type="password" onChange={onChange} name="password" placeholder="Enter your password" /></div>
-                        <div><input className="input-field shadow-sm" type="password" onChange={onChange} name="cpassword" placeholder="Confirm password" /></div>
-                        <div className="sign-up-div"> <button type="submit" class="btn signup-btn">Sign Up</button></div>
+                        <div>
+                            <input className="input-field shadow-sm" type="text" onChange={onChange} name="ownerName" placeholder="Enter your name" />
+                            { error&&credentials.ownerName.length===0 ? <label htmlFor="" className="errorLabel">Name can't be empty</label> : "" }
+                        </div>
+                        <div>
+                            <input className="input-field shadow-sm" type="email" onChange={onChange} name="email" placeholder="Enter your email" />
+                            { error&&credentials.email.length===0 ? <label htmlFor="" className="errorLabel">Email can't be empty</label> : "" }
+                        </div>
+                        <div>
+                            <input className="input-field shadow-sm" type="password" onChange={onChange} name="password" placeholder="Enter your password" />
+                            { error&&credentials.password.length===0 ? <label htmlFor="" className="errorLabel">Password can't be empty</label> : "" }
+                        </div>
+                        <div>
+                            <input className="input-field shadow-sm" type="password" onChange={onChange} name="cpassword" placeholder="Confirm password" />
+                            { error&&credentials.cpassword.length===0 ? <label htmlFor="" className="errorLabel">Confirm password can't be empty</label> : "" }
+                            { passError&&(credentials.cpassword!==credentials.password)? 
+                                <label htmlFor="" className="errorLabel">Passwords do not match</label> : "" }
+                        </div>
+                        {loading && <Spinner />}
+                        <div className="sign-up-div"> <button type="submit" className="btn signup-btn">Sign Up</button></div>
+                        {invalidCredError.error ? <label htmlFor="" style={{marginTop: "10px"}} className="errorLabel">{invalidCredError.message}</label> : ""}
                     </form>
                     <p>or</p>
-                    <div className="google-div"><button class="btn google-btn">Continue with <i class="fa-brands fa-google"></i></button></div>
+                    <div className="google-div"><button className="btn google-btn">Continue with <i className="fa-brands fa-google"></i></button></div>
                     <Link to="/login" className="login-link">Already have an account? Log in here</Link>
                     </div>
             </div>
