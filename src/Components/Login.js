424ea80086a8efc2227d4cba5fd44fed392f614f
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-06-25 23:01:53 +0530

    Login complete

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,16 +1,24 @@
 import { React, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 import '../css/auth.css'
+import Spinner from "./Spinner";
 
 export default function SignIn() {
 
     const [credentials, setCredentials] = useState({email: '', password: ''})
+    const [error,setError]=useState(false);
+    const [loading, setLoading] = useState(false)
+    const [invalidCredError, setInvalidCredError] = useState({error: false, message: ''})
     let navigate = useNavigate()
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         // const data = new FormData(event.currentTarget);
-
+        if(credentials.email.length===0||credentials.password.length===0){
+            setError(true);
+            return;
+        }
+        setLoading(true);
         const response = await fetch("https://flavr.tech/owner/login", {
             method: "POST",
             headers: {
@@ -19,18 +27,23 @@ export default function SignIn() {
             body: JSON.stringify(({email: credentials.email, password: credentials.password}))
         })
         const json = await response.json()
-
+        setLoading(false)
         if(json.message==="Auth successful"){
             // save token and redirect to dashboard
             localStorage.setItem('token', json.token)
             navigate('/')
         } else {
-            alert("Invalid credentials")
+            if(json.message!==undefined && json.message!==null){
+                setInvalidCredError({error: true, message: json.message})
+            } else {
+                setInvalidCredError({error: true, message: "Internal Server error"})
+            }
         }
     };
 
     const onChange = (e) => {
         setCredentials({...credentials, [e.target.name]: e.target.value})
+        setInvalidCredError({error: false, message: ''})
     }
 
     return (
@@ -40,9 +53,17 @@ export default function SignIn() {
                     <h1 className="signup-head">FlavR</h1>
                     <h3>Sign In</h3>
                     <form action="" onSubmit={handleSubmit}>
-                        <div><input className="input-field shadow-sm" name='email' type="email" value={credentials.email} onChange={onChange} placeholder="Enter your email" /></div>
-                        <div><input className="input-field shadow-sm" name='password' type="password" value={credentials.password} onChange={onChange} placeholder="Enter your password" /></div>
+                        <div>
+                            <input className="input-field shadow-sm" name='email' type="email" value={credentials.email} onChange={onChange} placeholder="Enter your email" />
+                            { error&&credentials.email.length===0 ? <label htmlFor="" className="errorLabel">Email can't be empty</label> : "" }
+                        </div>
+                        <div>
+                            <input className="input-field shadow-sm" name='password' type="password" value={credentials.password} onChange={onChange} placeholder="Enter your password" />
+                            { error&&credentials.password.length===0 ? <label htmlFor="" className="errorLabel">Password can't be empty</label> : "" }
+                        </div>
+                        {loading && <Spinner />}
                         <div className="sign-up-div"> <button type="submit" className="btn signup-btn">Sign In</button></div>
+                        {invalidCredError.error ? <label htmlFor="" style={{marginTop: "10px"}} className="errorLabel">{invalidCredError.message}</label> : ""}
                     </form>
                     <p>or</p>
                     <div className="google-div"><button className="btn google-btn">Continue with <i className="fa-brands fa-google"></i></button></div>
