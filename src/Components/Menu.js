6e4ec4be5697a8f8d67e65315f6b25e9071fc60d
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-06-26 15:34:43 +0530

    Menu change

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Menu() {
   return (
-    <div>
+    <div className='container' style={{padding: 0, margin: 0}}>
       <h1>menu</h1>
     </div>
   )
