ccc56a45cfc5e0638bb22aed790e354b6f764bd4
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-06-26 22:58:39 +0530

    Fix category and menu item

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import '../css/category.css'
+import React from "react";
+import "../css/category.css";
 
 export default function Category() {
-  return (
-    <div className="cat">
-      <button className="">
-        <img src="https://i.imgur.com/Ru0cbPs.png" alt="img" />
-        <p>fries</p>
-      </button>
-    </div>
-  )
+    return (
+        <div className="cat">
+            <button className="">
+                <img src="https://i.imgur.com/Ru0cbPs.png" alt="img" />
+                <p>Fries</p>
+            </button>
+        </div>
+    );
 }
--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -1,27 +1,22 @@
-import React from 'react'
-import '../css/menuitem.css'
+import React from "react";
+import "../css/menuitem.css";
 
 export default function MenuItem() {
-  return (
-   
-    <div class="card feature-card shadow-sm border-0 mb-5">
-    <div class="row">
-    <div class="col-lg-3 img-div">
-        <img class="item-image" src="https://i.imgur.com/CkwB6CY.jpg" alt="grow together"/>
-      </div>
-      <div class="col-lg-1 veg">
-      <img className="veg-img" src="https://i.imgur.com/qFC9XwF.png" alt="veg" />
-      </div>
-      <div class="col-lg-7 des">
-        <h3 class="feature-heading">Original Masala Maggi</h3>
-        <h3>Rs. 45</h3>
-        <p>Generate meaningful discussions with your audience and build a strong, loyal community. 
-          Think of the insightful conversations you miss out on with a feedback form.</p>
-      </div>
-      <div class="col-lg-1 ">
-      <i className="fa-sharp fa-solid fa-pen icon"></i>
-      </div>
-    </div>
-  </div>
-  )
+    return (
+        <div className="card mb-3 shadow-sm" style={{maxWidth: "500px",margin: 40}}>
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="https://i.imgur.com/CkwB6CY.jpg" style={{height: "100%"}} className="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div className="col-md-7">
+                    <div className="card-body">
+                        <h5 className="card-title"><img src="https://i.imgur.com/qFC9XwF.png" style={{width: "25px"}} alt="" /> Original Masala Maggi</h5>
+                        <p className="card-text" style={{fontWeight: 600, fontSize: "20px", marginBottom: 0}}>₹ 45</p>
+                        <p className="card-text" style={{color: "grey", fontWeight: "500",textAlign: 'justify', fontSize: "14px"}}>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+                <div className="col-md-1"><i className="fa-sharp fa-solid fa-pen icon"></i></div>
+            </div>
+        </div>
+    );
 }
