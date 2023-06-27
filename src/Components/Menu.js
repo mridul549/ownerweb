92268fad0bbe8842dbd02f7126fa61ecdadc220d
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-06-27 21:00:56 +0530

    Menu complete

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -8,7 +8,7 @@ export default function Menu() {
         <div className="container-fluid my-5 main-div" style={{ padding: 0, margin: 0 }}>
             <div className="d-flex flex-row justify-content-start align-items-center">
                 <h2 className="categoryHead my-5">Categories </h2>
-                <i class="fa-solid fa-circle-plus fa-2xl plus" ></i>
+                <i class="fa-solid fa-circle-plus fa-2xl plus" style={{color: "#004932"}} ></i>
             </div>
             <div className="col container categoryContainer">
                 <div className="categoryScroll">
